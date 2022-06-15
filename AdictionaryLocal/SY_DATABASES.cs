ab225a0c2dce72cc8608cf89bbc7850642d73e86
EnzoGrubisa <aurasoft.enzo@gmail.com>
2022-06-15 09:06:47 +0200

    AdictionaryLocal add

---
+++ AdictionaryLocal/SY_DATABASES.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCSCS.Adictionary
+{
+    public class SY_DATABASES
+    {
+        public int SYCD_ID { get; set; }
+        public string SYCD_USERCODE { get; set; } // short DB name e.g. "BD1" or "B"
+        public string SYCD_COMPCODE { get; set; } // company name e.g. "KAMEND"
+        public string SYCD_YEAR { get; set; } // DB year
+        public string SYCD_DESCRIPTION { get; set; } // DB descriptioon
+        public string SYCD_DBASENAME { get; set; } // full DB name
+        public string SYCD_USERNAME { get; set; } // ??
+        public string SYCD_USERPSWD { get; set; } // ??
+    }
+}
---
+++ AdictionaryLocal/SY_INDEXES.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCSCS.Adictionary
+{
+    public class SY_INDEXES
+    {
+        public int SYKI_ID { get; set; }
+        public string SYKI_SCHEMA { get; set; } // shecma in which this index exists
+        public string SYKI_DESCRIPTION { get; set; } // index description
+        public string SYKI_KEYNAME { get; set; } // index name
+        public int SYKI_KEYNUM { get; set; } // index priority number for current schema
+        public string SYKI_FIELD { get; set; } // field for the index. There can be more than one field(segment) for one index
+        public string SYKI_UNIQUE { get; set; } // is the index unique? Y or N
+        public string SYKI_ASCDESC { get; set; } // sorting type for the current index. A or D(asc or desc)
+        public int? SYKI_SEGNUM { get; set; } // ordinal number of the segment(field) for the current index
+    }
+}
