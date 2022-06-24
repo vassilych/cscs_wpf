c90c857677b4c23c7bc5ce15c9e8a4c16f5c12a4
EnzoGrubisa <aurasoft.enzo@gmail.com>
2022-06-24 13:55:21 +0200

    a

---
+++ NavigatorClass.cs
@@ -0,0 +1,82 @@
+﻿using SplitAndMerge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfControlLibrary1;
+
+namespace WpfCSCS
+{
+    public class NavigatorClass
+    {
+        public static void Init()
+        {
+            ParserFunction.RegisterFunction(Constants.NAVIGATOR, new NavigatorFunction()); 
+        }
+
+        public static Dictionary<string, int> navigatorOfTable = new Dictionary<string, int>();
+        public static Dictionary<string, string> navigatorKey = new Dictionary<string, string>();
+
+        public class NavigatorFunction : ParserFunction
+        {
+            string navigatorName;
+            int tableHndlNum;
+            static string tableKey;
+            
+            //static KeyClass KeyClass;
+
+            static NavigatorButton navigatorButton;
+
+            protected override Variable Evaluate(ParsingScript script)
+            {
+                List<Variable> args = script.GetFunctionArgs();
+                Utils.CheckArgs(args.Count, 3, m_name);
+                navigatorName = Utils.GetSafeString(args, 0);
+                tableHndlNum = Utils.GetSafeInt(args, 1);
+                tableKey = Utils.GetSafeString(args, 2).ToLower(); // e.g. VEZM_RNALOGLIN
+
+                navigatorOfTable.Add(navigatorName, tableHndlNum);
+                navigatorKey.Add(navigatorName, tableKey);
+
+                //new Btrieve.FINDVClass(tableHndlNum, operationType, tableKey, matchExactValue, forString, columnsToSelect, script, keyo).FINDV();
+
+                return Variable.EmptyInstance;
+            }
+        }
+
+        public static void NavigateFirst(string navigatorName)
+        {
+            new Btrieve.FINDVClass(navigatorOfTable[navigatorName], "f", navigatorKey[navigatorName]).FINDV();
+        }
+        public static void NavigateLast(string navigatorName)
+        {
+            new Btrieve.FINDVClass(navigatorOfTable[navigatorName], "l", navigatorKey[navigatorName]).FINDV();
+        }
+
+        public static void NavigateNext(string navigatorName)
+        {
+            if(Btrieve.OPENVs[navigatorOfTable[navigatorName]].currentRow == 0)
+            {
+                NavigateFirst(navigatorName);
+            }
+            else
+            {
+                new Btrieve.FINDVClass(navigatorOfTable[navigatorName], "n").FINDV();
+            }
+        }
+
+        public static void NavigatePrevious(string navigatorName)
+        {
+            if (Btrieve.OPENVs[navigatorOfTable[navigatorName]].currentRow == 0)
+            {
+                NavigateFirst(navigatorName);
+            }
+            else
+            {
+                new Btrieve.FINDVClass(navigatorOfTable[navigatorName], "p").FINDV();
+            }
+        }
+
+    }
+}
