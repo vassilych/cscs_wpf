a6e4e3ba647f50ef9b374091176b9185dbf01cd2
EnzoGrubisa <aurasoft.enzo@gmail.com>
2023-06-13 14:16:05 +0200

    13.06.2023.

--- CSCS_GUI.cs
+++ CSCS_GUI.cs
@@ -6,14 +6,12 @@ using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
-using System.Drawing.Printing;
 using System.Dynamic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -314,7 +312,7 @@ namespace SplitAndMerge
 
 namespace WpfCSCS
 {
-    
+
     public class CSCS_GUI
     {
         public NavigatorClass NavigatorClass { get; set; } = new NavigatorClass();
